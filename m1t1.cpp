d5dd1d1a9830f8ec996078af7af7cf5fe888ee21
Austin Wadkins <166045987+Reaper3825@users.noreply.github.com>
2025-08-25 14:58:12 +0000

    Add initial implementation of greeting program

---
+++ m1t1.cpp
@@ -0,0 +1,18 @@
+/*
+m1t1: hello, user
+Wadkinsa, 8/25/25
+INPUT: run the program
+process: None Yet
+OUTPUT: return 0
+SPECIAL FX: Greet the user
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{ cout < "Hello" << endl;
+    return 0;
+// output
+return 0; // no errors
+} 
